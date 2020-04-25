5f7c69afad9c7ac72a0df9557e1862e806585dc8
AminataDiallo <minadialloo5@gmail.com>
2020-04-25 15:31:16 +0000

    initialisation du projet de devellopement d'une application java desktop pour la gestion des journeaux

---
+++ src/java_application_journal_management/Java_application_journal_management.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_application_journal_management;
+
+/**
+ *
+ * @author HP USER
+ */
+public class Java_application_journal_management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
